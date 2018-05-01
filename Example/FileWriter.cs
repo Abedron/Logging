a20f86c0c1f9002eb28d6f677dd3ebc691628cf6
transglobals <transglobals@gmail.com>
2018-05-01 10:29:26 +0200

    FileWriter example project

---
+++ Example/FileWriter.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Text;
+using System.Timers;
+
+namespace Logging.Writers
+{
+    public class FileWriter : Writer
+    {
+        private StringBuilder stringBuilder;
+        private Timer timer;
+        private bool recorded;
+        private string path;
+
+        public FileWriter(LogLevel logLevel) : base(logLevel)
+        {
+            path = Directory.GetCurrentDirectory() + "/log.txt";
+            
+            stringBuilder = new StringBuilder(100);
+            
+            timer = new Timer(1000);
+            timer.Start();
+            timer.Elapsed += TimerHandler;
+        }
+
+        public override void Debug<S>(S message)
+        {
+            stringBuilder.Append("Debug: " + message + "\n");
+            recorded = true;
+        }
+
+        public override void Warning<S>(S message)
+        {
+            stringBuilder.Append("Warning: " + message + "\n");
+            recorded = true;
+        }
+
+        public override void Error<S>(S message)
+        {
+            stringBuilder.Append("Error: " + message + "\n");
+            recorded = true;
+        }
+
+        private void TimerHandler(object sender, ElapsedEventArgs e)
+        {
+            if (recorded)
+            {
+                File.AppendAllText(path, stringBuilder.ToString());
+                stringBuilder.Clear();
+                recorded = false;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            stringBuilder.Clear();
+            timer.Dispose();
+        }
+    }
+}
---
+++ Example/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Logging.Writers;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileWriter fileWriter = new FileWriter(LogLevel.Debug | LogLevel.Error | LogLevel.Warning);
+            Log.Logger.AddWriter(fileWriter);
+            
+            Log.Debug("Hello World!");
+            
+            Log.Warning("Hello Mars!");
+            
+            Log.Error("Hello Europe!");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                Log.Error(message, "Info");
+            }
+        }
+    }
+}
